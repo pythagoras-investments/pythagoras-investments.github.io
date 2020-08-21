f964819d8193b59a25f8b915f4baffd39ff3b11f
Pythagoras <70032957+pythagoras-investments@users.noreply.github.com>
2020-08-21 15:33:31 -0400

    Update email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,5 +1,5 @@
 // fn for bot harvest busting
 
 $('a[data-mail]').on('click', function() {
-        window.location = 'mailto:' + $(this).data('mail')+'@'+ $(this).data('domain')+'?subject=Inquiry from Pythagoras Website';
+        window.location = 'mailto:' + $(this).data('mail')+'@'+ $(this).data('domain')+'?subject='+$(this).data('subject');
 });
