5015f6d8341756e84519f419bb366a1b0ce47bb8
Pythagoras <70032957+pythagoras-investments@users.noreply.github.com>
2020-08-23 00:58:01 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,32 +7,32 @@ var BeautifulJekyllJS = {
 
   init : function() {
     // Set the navbar-dark/light class based on its background color
-    const rgb = $('.navbar').css("background-color").replace(/[^\d,]/g,'').split(",");
-    const brightness = Math.round(( // http://www.w3.org/TR/AERT#color-contrast
-      parseInt(rgb[0]) * 299 +
-      parseInt(rgb[1]) * 587 +
-      parseInt(rgb[2]) * 114
-    ) / 1000);
-    if (brightness <= 125) {
-      $(".navbar").removeClass("navbar-light").addClass("navbar-dark");
-    }
+//     const rgb = $('.navbar').css("background-color").replace(/[^\d,]/g,'').split(",");
+//     const brightness = Math.round(( // http://www.w3.org/TR/AERT#color-contrast
+//       parseInt(rgb[0]) * 299 +
+//       parseInt(rgb[1]) * 587 +
+//       parseInt(rgb[2]) * 114
+//     ) / 1000);
+//     if (brightness <= 125) {
+//       $(".navbar").removeClass("navbar-light").addClass("navbar-dark");
+//     }
 
     // Shorten the navbar after scrolling a little bit down
-    $(window).scroll(function() {
-        if ($(".navbar").offset().top > 50) {
-            $(".navbar").addClass("top-nav-short");
-        } else {
-            $(".navbar").removeClass("top-nav-short");
-        }
-    });
+//     $(window).scroll(function() {
+//         if ($(".navbar").offset().top > 50) {
+//             $(".navbar").addClass("top-nav-short");
+//         } else {
+//             $(".navbar").removeClass("top-nav-short");
+//         }
+//     });
 
     // On mobile, hide the avatar when expanding the navbar menu
-    $('#main-navbar').on('show.bs.collapse', function () {
-      $(".navbar").addClass("top-nav-expanded");
-    });
-    $('#main-navbar').on('hidden.bs.collapse', function () {
-      $(".navbar").removeClass("top-nav-expanded");
-    });
+//     $('#main-navbar').on('show.bs.collapse', function () {
+//       $(".navbar").addClass("top-nav-expanded");
+//     });
+//     $('#main-navbar').on('hidden.bs.collapse', function () {
+//       $(".navbar").removeClass("top-nav-expanded");
+//     });
 
     // show the big header image
     BeautifulJekyllJS.initImgs();
